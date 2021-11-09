bde01386bf74b4c334fb12916f5d8e9d2ab2ab8e
niki <nikolayp1707@gmail.com>
2021-11-09 17:46:54 +0200

    add module.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Logo from "/src/components/logo/logo";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
+import styles from "./Header.module.scss";
+import classNames from "classnames";
+import Container from "@mui/material/Container";
 export default function Header(){
     return(
+      <div className={classNames(styles.wrapper)}>
+       <Container className={classNames(styles.container)} maxWidth="x1">
         <Grid container spacing={2}>
             <Grid item xs={3}>
                 <Logo />
@@ -29,9 +35,10 @@ export default function Header(){
             <Grid item xs={0}>
                 <Button>Home</Button>
                 <Button>Activity</Button>
-                <Button variant="contained">Explore</Button>
+                <Button variant="contained" className={classNames(styles.Button)}>Explore</Button>
             </Grid>
         </Grid>
-       
-    )
+       </Container>
+      </div>
+    );
 }
