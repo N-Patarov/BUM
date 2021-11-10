d4008c0363cfca5b24498a1410d1d664daaed704
niki <nikolayp1707@gmail.com>
2021-11-10 14:43:48 +0200

    move to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import Example from "../src/components/example/Example";
 import Logo from "../src/components/logo/logo";
-import Header from "../src/components/header/Header"
+
+import Header from "../src/components/header/Header.jsx"
 
 export default function Index() {
+  return <Header />;
   return <Example />;
 }
