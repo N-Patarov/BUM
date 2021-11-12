ad5470e76a5de6be1c2ffefe10d893cc8730c8e7
niki <nikolayp1707@gmail.com>
2021-11-12 23:14:03 +0200

    try by removing example

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Example from "../src/components/example/Example";
-import Logo from "../src/components/logo/logo";
-import Avatar from "../src/components/avatar/Avatar.jsx";
+
 import Header from "../src/components/header/Header.jsx";
 
 export default function Index() {
   return( 
     <div>
       <Header />
-      <Avatar />
-      <Example />
     </div>
   );
 }
