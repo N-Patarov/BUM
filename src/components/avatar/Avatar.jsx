ad8704674493ccfc2fbefa3ee91ec118cb2009cb
niki <nikolayp1707@gmail.com>
2021-11-12 18:58:01 +0200

    undeclare avatar and header

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -5,8 +5,8 @@ import classNames from "classnames";
 export default function Avatar({size = 90, url='images/avatar.png', verified=false}){
     
     return(
-        <div className={classNames(styles.avatar)}>
-            <img className={classNames(styles.image)} size={size} src={url}></img>
+        <div width={size} height={size} className={classNames(styles.avatar)}>
+            <img className={classNames(styles.image)} width={size} height={size} src={url}></img>
             <img className={classNames(styles.badge)} src= {verified == true ? "images/verified.svg" : ""}></img>
         </div>
     )
