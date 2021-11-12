351000dced5703c1fe48faebfdd08dfc289152d5
niki <nikolayp1707@gmail.com>
2021-11-12 23:05:52 +0200

    add them again

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Example from "../src/components/example/Example";
 import Logo from "../src/components/logo/logo";
-
+import Avatar from "../src/components/avatar/Avatar.jsx";
+import Header from "../src/components/header/Header.jsx";
 
 export default function Index() {
   return( 
     <div>
+      <Header />
+      <Avatar />
       <Example />
     </div>
   );
