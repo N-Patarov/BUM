49b35822a88d39a0a7415a536d1b5f2f9f1866b3
niki <nikolayp1707@gmail.com>
2021-11-15 11:11:51 +0200

    add width and height

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from "../src/components/header/Header.jsx";
+import Avatar from "../src/components/avatar/Avatar.jsx";
 
 export default function Index() {
   return( 
     <div>
       <Header />
+      <Avatar />
     </div>
   );
 }
