9a3298e24d489a1654f8f129b2730cffe2fc11ba
niki <nikolayp1707@gmail.com>
2021-11-17 10:14:17 +0200

    add currency

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -12,13 +12,16 @@ import millify from 'millify';
 export default function Card({name = "asd", likes = 0, user = {}, mediaUrl = "", price = 0, currency = "", timeLeft = 0}){
     return(
         <MuiCard className={classNames(styles.card)}>
-            <CardHeader className={classNames(styles.avatar)} avatar={<Avatar size={33} url={user.avatarUrl} verified={user.verified}/>} />
+            <CardHeader className={classNames(styles.avatar)} avatar={
+            <Avatar size={33} url={user.avatarUrl} verified={user.verified}/>
+            }>
+            </CardHeader>
             <CardMedia className={classNames(styles.media)} picture={mediaUrl}>
                 media
             </CardMedia>
             <CardContent className={classNames(styles.content)}>
                 <div className={classNames(styles.title)}>{name}</div>
-                <div className={classNames(styles.price)} currency={currency}>~{price}</div>
+                <div className={classNames(styles.price)}>~{price} {currency}</div>
                 <Chip className={classNames(styles.likes)} icon={<FavoriteIcon />} label={millify(likes)} variant="outlined" />
             </CardContent>
         </MuiCard>
