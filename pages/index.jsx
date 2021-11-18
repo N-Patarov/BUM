546e081b7fa53ef3ca728186720834cd97fb5728
niki <nikolayp1707@gmail.com>
2021-11-18 16:13:16 +0200

    trending component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from "../src/components/header/Header.jsx";
 import Card from "../src/components/card/Card.jsx";
+import Trending from "../src/components/trending/Trending.jsx";
 
 export default function Index() {
   return( 
     <div>
       <Header />
       <Card />
+      <Trending />
     </div>
   );
 }
